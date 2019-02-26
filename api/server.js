910e9b4c619c8f78809f61660b6e51df0c0f11c2
Sunaina Asif <sunaina.aasif@gmail.com>
2019-02-26 15:32:51 +0500

    This is an example of React CRUD Operations

--- api/server.js
+++ api/server.js
@@ -3,6 +3,13 @@ const app=express();
 const bodyParser=require('body-parser');
 const PORT=4000;
 const cors=require('cors');
+const config=require('./DB');
+
+mongoose.Promise=global.Promise;
+mongoose.connect(config.DB, { useNewUrlParser :true }).then(
+    ()=>{console.log('Database is Connected')},
+    err => {console.log("Can not connect to the database")}
+)
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -10,3 +17,4 @@ app.use(bodyParser.json());
 
 
 app.listen(PORT, ()=>{console.log(`Server is running on port ${PORT}`)})
+
