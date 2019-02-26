39506267b379d92aed8c037cfdcb74a7f4594afd
Sunaina Asif <sunaina.aasif@gmail.com>
2019-02-26 15:46:40 +0500

    react - crud

--- api/server.js
+++ api/server.js
@@ -16,5 +16,5 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 
-app.listen(PORT, ()=>{console.log(`Server is running on port ${PORT}`)})
+app.listen(PORT , ()=>{console.log(`Server is running on port ${PORT}`)})
 
